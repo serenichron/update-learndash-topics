6ffbc6c4def11d3179a62715e193f854b53d29b0
Vlad Tudorie <vlad@serenichron.com>
2024-12-12 20:38:05 +0200

    2.0.5-aplha: This updated version should address the issues we observed:
    It properly handles attributes and classes, preventing duplicate class= declarations.
    It correctly processes special attributes like fb_built and column_structure.
    It removes the problematic numbering system for classes.
    It properly escapes attribute values, preventing issues with quotes.
    It correctly handles module-specific inner divs.

--- includes/class-content-processor.php
+++ includes/class-content-processor.php
@@ -29,47 +29,75 @@ class TSTPrep_CC_Content_Processor {
         </div>';
     
         // Convert Divi sections
-        $content = preg_replace('/\[et_pb_section([^\]]*)\](.*?)\[\/et_pb_section\]/s', '<div class="et_pb_section et_pb_section_0 et_section_regular$1">$2</div>', $content);
-    
-        // Convert Divi rows
-        $content = preg_replace('/\[et_pb_row([^\]]*)\](.*?)\[\/et_pb_row\]/s', '<div class="et_pb_row et_pb_row_0$1">$2</div>', $content);
-    
-        // Convert Divi columns
-        $content = preg_replace('/\[et_pb_column([^\]]*)\](.*?)\[\/et_pb_column\]/s', '<div class="et_pb_column$1">$2</div>', $content);
-    
-        // Convert Divi text modules
-        $content = preg_replace('/\[et_pb_text([^\]]*)\](.*?)\[\/et_pb_text\]/s', '<div class="et_pb_module et_pb_text$1"><div class="et_pb_text_inner">$2</div></div>', $content);
-    
-        // Convert Divi code modules
-        $content = preg_replace('/\[et_pb_code([^\]]*)\](.*?)\[\/et_pb_code\]/s', '<div class="et_pb_module et_pb_code$1"><div class="et_pb_code_inner">$2</div></div>', $content);
-    
-        // Convert Divi sidebar modules
-        $content = preg_replace('/\[et_pb_sidebar([^\]]*)\](.*?)\[\/et_pb_sidebar\]/s', '<div class="et_pb_module et_pb_sidebar$1">$2</div>', $content);
-    
-        // Process module attributes
-        $content = preg_replace_callback('/class="([^"]*)"/', function($matches) {
-            $classes = explode(' ', $matches[1]);
-            $classes[] = 'et_pb_text_align_left';
-            $classes[] = 'et_pb_bg_layout_light';
-            return 'class="' . implode(' ', array_unique($classes)) . '"';
+        $content = preg_replace_callback('/\[et_pb_section([^\]]*)\](.*?)\[\/et_pb_section\]/s', function($matches) {
+            $attrs = self::parse_attributes($matches[1]);
+            $classes = ['et_pb_section', 'et_pb_section_0', 'et_section_regular'];
+            if (isset($attrs['fb_built'])) $classes[] = 'et_had_animation';
+            $attr_string = self::build_attributes($attrs, $classes);
+            return "<div {$attr_string}>{$matches[2]}</div>";
         }, $content);
     
-        // Add module numbering
+        // Convert Divi rows
+        $content = preg_replace_callback('/\[et_pb_row([^\]]*)\](.*?)\[\/et_pb_row\]/s', function($matches) {
+            $attrs = self::parse_attributes($matches[1]);
+            $classes = ['et_pb_row', 'et_pb_row_0'];
+            if (isset($attrs['column_structure'])) $classes[] = 'et_pb_row_' . str_replace(',', '_', $attrs['column_structure']);
+            $attr_string = self::build_attributes($attrs, $classes);
+            return "<div {$attr_string}>{$matches[2]}</div>";
+        }, $content);
+    
+        // Convert Divi columns
+        $content = preg_replace_callback('/\[et_pb_column([^\]]*)\](.*?)\[\/et_pb_column\]/s', function($matches) {
+            $attrs = self::parse_attributes($matches[1]);
+            $classes = ['et_pb_column'];
+            if (isset($attrs['type'])) $classes[] = 'et_pb_column_' . $attrs['type'];
+            $attr_string = self::build_attributes($attrs, $classes);
+            return "<div {$attr_string}>{$matches[2]}</div>";
+        }, $content);
+    
+        // Convert Divi modules (text, code, sidebar)
         $module_types = ['et_pb_text', 'et_pb_code', 'et_pb_sidebar'];
         foreach ($module_types as $type) {
-            $count = 0;
-            $content = preg_replace_callback('/class="([^"]*)'.$type.'([^"]*)"/', function($matches) use (&$count) {
-                $count++;
-                return 'class="'.$matches[1].$matches[0].'_'.$count.'"';
+            $content = preg_replace_callback('/\['.$type.'([^\]]*)\](.*?)\[\/'.$type.'\]/s', function($matches) use ($type) {
+                $attrs = self::parse_attributes($matches[1]);
+                $classes = ['et_pb_module', $type];
+                $attr_string = self::build_attributes($attrs, $classes);
+                $inner_class = $type . '_inner';
+                return "<div {$attr_string}><div class='{$inner_class}'>{$matches[2]}</div></div>";
             }, $content);
         }
     
         // Handle video wrapper
         $content = preg_replace('/<iframe([^>]*)><\/iframe>/', '<div class="fluid-width-video-wrapper" style="padding-top: 56.25%;"><iframe$1></iframe></div>', $content);
     
-        // Clean up any empty paragraphs that Divi might have added
+        // Clean up any empty paragraphs
         $content = preg_replace('/<p>\s*<\/p>/', '', $content);
     
         return $content;
+    }
+    
+    private static function parse_attributes($attr_string) {
+        $attrs = [];
+        preg_match_all('/(\w+)="([^"]*)"/', $attr_string, $matches, PREG_SET_ORDER);
+        foreach ($matches as $match) {
+            $attrs[$match[1]] = $match[2];
+        }
+        return $attrs;
+    }
+    
+    private static function build_attributes($attrs, $classes) {
+        if (isset($attrs['module_class'])) {
+            $classes = array_merge($classes, explode(' ', $attrs['module_class']));
+            unset($attrs['module_class']);
+        }
+        $classes[] = 'et_pb_text_align_left';
+        $classes[] = 'et_pb_bg_layout_light';
+        $attr_string = 'class="' . implode(' ', array_unique($classes)) . '"';
+        foreach ($attrs as $key => $value) {
+            if ($key !== 'type' && $key !== 'column_structure') {
+                $attr_string .= " {$key}=\"{$value}\"";
+            }
+        }
+        return $attr_string;
     }
 }
