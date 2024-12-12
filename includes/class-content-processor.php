c0f7a0c36be69fa79a6e8e400779d5c50557fcab
Vlad Tudorie <vlad@serenichron.com>
2024-12-12 20:16:07 +0200

    2.0.3-alpha: preserving Divi IDs

--- includes/class-content-processor.php
+++ includes/class-content-processor.php
@@ -19,20 +19,26 @@ class TSTPrep_CC_Content_Processor {
     }
 
     private static function divi_to_html_cleanup($content) {
-        // Remove outer Divi section and row
-        $content = preg_replace('/\[et_pb_section.*?\](.*)\[\/et_pb_section\]/s', '$1', $content);
-        $content = preg_replace('/\[et_pb_row.*?\](.*)\[\/et_pb_row\]/s', '$1', $content);
+        // Remove outer Divi section and row, but preserve IDs
+        $content = preg_replace('/\[et_pb_section.*?module_id="([^"]*)".*?\](.*)\[\/et_pb_section\]/s', '<div id="$1">$2</div>', $content);
+        $content = preg_replace('/\[et_pb_row.*?module_id="([^"]*)".*?\](.*)\[\/et_pb_row\]/s', '<div id="$1">$2</div>', $content);
     
-        // Convert Divi columns to div elements
+        // Convert Divi columns to div elements, preserving type, module_class, and module_id
+        $content = preg_replace('/\[et_pb_column.*?type="([^"]*)".*?module_class="([^"]*)".*?module_id="([^"]*)".*?\](.*?)\[\/et_pb_column\]/s', '<div class="et_pb_column et_pb_column_$1 $2" id="$3">$4</div>', $content);
+    
+        // Convert Divi text modules to div elements, preserving module_class and module_id
+        $content = preg_replace('/\[et_pb_text.*?module_class="([^"]*)".*?module_id="([^"]*)".*?\](.*?)\[\/et_pb_text\]/s', '<div class="et_pb_text $1" id="$2">$3</div>', $content);
+    
+        // Convert Divi code modules to div elements, preserving module_id
+        $content = preg_replace('/\[et_pb_code.*?module_id="([^"]*)".*?\](.*?)\[\/et_pb_code\]/s', '<div class="et_pb_code" id="$1">$2</div>', $content);
+    
+        // Convert Divi sidebar modules to div elements, preserving module_id
+        $content = preg_replace('/\[et_pb_sidebar.*?module_id="([^"]*)".*?\](.*?)\[\/et_pb_sidebar\]/s', '<div class="et_pb_sidebar" id="$1">$2</div>', $content);
+    
+        // Handle cases where module_id might not be present
         $content = preg_replace('/\[et_pb_column.*?type="([^"]*)".*?module_class="([^"]*)".*?\](.*?)\[\/et_pb_column\]/s', '<div class="et_pb_column et_pb_column_$1 $2">$3</div>', $content);
-    
-        // Convert Divi text modules to div elements
         $content = preg_replace('/\[et_pb_text.*?module_class="([^"]*)".*?\](.*?)\[\/et_pb_text\]/s', '<div class="et_pb_text $1">$2</div>', $content);
-    
-        // Convert Divi code modules to div elements
         $content = preg_replace('/\[et_pb_code.*?\](.*?)\[\/et_pb_code\]/s', '<div class="et_pb_code">$1</div>', $content);
-    
-        // Convert Divi sidebar modules to div elements
         $content = preg_replace('/\[et_pb_sidebar.*?\](.*?)\[\/et_pb_sidebar\]/s', '<div class="et_pb_sidebar">$1</div>', $content);
     
         // Remove any remaining shortcode brackets
@@ -42,5 +48,5 @@ class TSTPrep_CC_Content_Processor {
         $content = preg_replace('/<p>\s*<\/p>/', '', $content);
     
         return $content;
-        }
+    }
 }
