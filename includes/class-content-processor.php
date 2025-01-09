0b890b1df73dd03bdab3e66493389a077dc8cdfd
Robert Niculescu <robert@dacianempire.com>
2025-01-09 17:41:40 +0200

    added additional support and fixed various issues

--- includes/class-content-processor.php
+++ includes/class-content-processor.php
@@ -2,8 +2,6 @@
 class TSTPrep_CC_Content_Processor {
     public static function process_content($content, $cleanup_type) {
         switch ($cleanup_type) {
-            case 'shortcode':
-                return self::shortcode_cleanup($content);
             case 'divi_to_html':
                 $cleaned_content = self::divi_to_html_cleanup($content);
                 // Ensure the wrappers are only added if they don't already exist
@@ -15,16 +13,9 @@ class TSTPrep_CC_Content_Processor {
                 return $content;
         }
     }
-    
-    private static function shortcode_cleanup($content) {
-        // Preserve the existing shortcode cleanup logic
-        $pattern = '/\[([^\]]*)\]/';
-        preg_match($pattern, $content, $matches);
-        return isset($matches[0]) ? $matches[0] : '';
-    }
 
     private static function divi_to_html_cleanup($content) {
-        // Preserve outer structure with the new wrapper
+        // Wrap content in Divi outer structure
         $content = '<div class="et-db et_divi_builder"><div id="et-boc" class="et-boc">
             <div id="et_builder_outer_content" class="et_builder_outer_content">
                 <div class="et-l et-l--post">
@@ -32,37 +23,53 @@ class TSTPrep_CC_Content_Processor {
                 </div>
             </div>
         </div></div>';
-    
-        // Convert Divi sections
-        $content = preg_replace_callback('/\[et_pb_section([^\]]*)\](.*?)\[\/et_pb_section\]/s', function($matches) {
+
+        // Process all known Divi shortcodes
+        $content = self::process_divi_shortcodes($content);
+
+        // Remove any empty paragraphs
+        $content = preg_replace('/<p>\s*<\/p>/', '', $content);
+
+        return $content;
+    }
+
+    private static function process_divi_shortcodes($content) {
+        // Process [et_pb_section]
+        $content = preg_replace_callback('/\[et_pb_section([^\]]*)\](.*?)\[\/et_pb_section\]/s', function ($matches) {
             static $section_count = 0;
-            $section_count++;
             $attrs = self::parse_attributes($matches[1]);
             $classes = ['et_pb_section', "et_pb_section_{$section_count}", 'et_section_regular'];
             if (!isset($attrs['fullwidth']) || $attrs['fullwidth'] !== 'on') {
                 $classes[] = 'et_section_transparent';
             }
             $attr_string = self::build_attributes($attrs, $classes);
-            return "<div {$attr_string}>{$matches[2]}</div>";
+            $inner_content = self::process_divi_shortcodes($matches[2]); // Recursive call
+            $output = "<div {$attr_string}>{$inner_content}</div>";
+            $section_count++;
+            return $output;
         }, $content);
-    
-        // Convert Divi rows
-        $content = preg_replace_callback('/\[et_pb_row([^\]]*)\](.*?)\[\/et_pb_row\]/s', function($matches) {
+
+        // Process [et_pb_row]
+        $content = preg_replace_callback('/\[et_pb_row([^\]]*)\](.*?)\[\/et_pb_row\]/s', function ($matches) {
             static $row_count = 0;
-            $row_count++;
             $attrs = self::parse_attributes($matches[1]);
             $classes = ['et_pb_row', "et_pb_row_{$row_count}"];
+            if (isset($attrs['module_class'])) {
+                $classes[] = $attrs['module_class'];
+            }
             if (isset($attrs['column_structure'])) {
                 $classes[] = 'et_pb_row_' . str_replace(',', '_', $attrs['column_structure']);
             }
             $attr_string = self::build_attributes($attrs, $classes);
-            return "<div {$attr_string}>{$matches[2]}</div>";
+            $inner_content = self::process_divi_shortcodes($matches[2]); // Recursive call
+            $output = "<div {$attr_string}>{$inner_content}</div>";
+            $row_count++;
+            return $output;
         }, $content);
-    
-        // Convert Divi columns
-        $content = preg_replace_callback('/\[et_pb_column([^\]]*)\](.*?)\[\/et_pb_column\]/s', function($matches) {
+
+        // Process [et_pb_column]
+        $content = preg_replace_callback('/\[et_pb_column([^\]]*)\](.*?)\[\/et_pb_column\]/s', function ($matches) {
             static $column_count = 0;
-            $column_count++;
             $attrs = self::parse_attributes($matches[1]);
             $classes = ['et_pb_column', "et_pb_column_{$attrs['type']}", "et_pb_column_{$column_count}"];
             $classes[] = 'et_pb_css_mix_blend_mode_passthrough';
@@ -70,38 +77,68 @@ class TSTPrep_CC_Content_Processor {
                 $classes[] = 'et-last-child';
             }
             $attr_string = self::build_attributes($attrs, $classes);
-            return "<div {$attr_string}>{$matches[2]}</div>";
+            $inner_content = self::process_divi_shortcodes($matches[2]); // Recursive call
+            $output = "<div {$attr_string}>{$inner_content}</div>";
+            $column_count++;
+            return $output;
         }, $content);
-    
-        // Convert Divi modules (text, code, sidebar)
-        $module_types = ['et_pb_text', 'et_pb_code', 'et_pb_sidebar'];
+
+        // Process Divi modules (e.g., [et_pb_text], [et_pb_toggle], [et_pb_code], etc.)
+        $module_types = ['et_pb_text', 'et_pb_code', 'et_pb_sidebar', 'et_pb_toggle', 'et_pb_video'];
         foreach ($module_types as $type) {
             static $module_counts = [];
             if (!isset($module_counts[$type])) {
                 $module_counts[$type] = 0;
             }
-            $content = preg_replace_callback('/\['.$type.'([^\]]*)\](.*?)\[\/'.$type.'\]/s', function($matches) use ($type, &$module_counts) {
-                $module_counts[$type]++;
+            $content = preg_replace_callback('/\[' . preg_quote($type, '/') . '([^\]]*)\](.*?)\[\/' . preg_quote($type, '/') . '\]/s', function ($matches) use ($type, &$module_counts) {
                 $attrs = self::parse_attributes($matches[1]);
                 $classes = ['et_pb_module', $type, "{$type}_{$module_counts[$type]}", 'et_pb_text_align_left', 'et_pb_bg_layout_light'];
-                $attr_string = self::build_attributes($attrs, $classes);
-                $inner_content = $matches[2];
-                if ($type === 'et_pb_sidebar') {
-                    return "<div {$attr_string}></div>";
+            
+                // Recursively process inner content
+                $inner_content = self::process_divi_shortcodes($matches[2]);
+            
+                if ($type === 'et_pb_video') {
+                    // Handle YouTube video embedding
+                    if (isset($attrs['src']) && preg_match('/youtu(?:\.be|be\.com)\/(?:watch\?v=|embed\/|)([^\s&?]+)/', $attrs['src'], $video_match)) {
+                        $video_id = $video_match[1];
+                        $iframe = "<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/{$video_id}?feature=oembed\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+                        $module_counts[$type]++;
+                        return "<div style=\"text-align:center;\">{$iframe}</div>";
+                    }
+                    return ''; // If no valid video URL is found, return nothing
                 }
-                return "<div {$attr_string}><div class='{$type}_inner'>{$inner_content}</div></div>";
-            }, $content);
+            
+                if ($type === 'et_pb_toggle') {
+                    // Extract the title attribute (default to "Toggle Title" if not set)
+                    $title = isset($attrs['title']) ? htmlspecialchars($attrs['title'], ENT_QUOTES, 'UTF-8') : 'Toggle Title';
+            
+                    // Add additional classes specific to toggle
+                    $classes[] = 'et_pb_toggle_item';
+                    $classes[] = 'et_pb_toggle_close'; // Default state is closed
+            
+                    // Build the HTML structure for the toggle
+                    $output = "
+                        <div " . self::build_attributes($attrs, $classes) . ">
+                            <h5 class='et_pb_toggle_title'>{$title}</h5>
+                            <div class='et_pb_toggle_content clearfix'>{$inner_content}</div>
+                        </div>";
+            
+                    $module_counts[$type]++;
+                    return $output;
+                }
+            
+                // Generic handling for other module types
+                $attr_string = self::build_attributes($attrs, $classes);
+                $output = "<div {$attr_string}><div class='{$type}_inner'>{$inner_content}</div></div>";
+            
+                $module_counts[$type]++;
+                return $output;
+            }, $content);            
         }
-    
-        // Handle video wrapper
-        $content = preg_replace('/<iframe([^>]*)><\/iframe>/', '<div class="fluid-width-video-wrapper"><iframe$1></iframe></div>', $content);
-    
-        // Clean up any empty paragraphs
-        $content = preg_replace('/<p>\s*<\/p>/', '', $content);
-    
+
         return $content;
     }
-    
+
     private static function parse_attributes($attr_string) {
         $attrs = [];
         preg_match_all('/(\w+)="([^"]*)"/', $attr_string, $matches, PREG_SET_ORDER);
@@ -110,14 +147,14 @@ class TSTPrep_CC_Content_Processor {
         }
         return $attrs;
     }
-    
+
     private static function build_attributes($attrs, $classes) {
         if (isset($attrs['module_class'])) {
             $classes = array_merge($classes, explode(' ', $attrs['module_class']));
             unset($attrs['module_class']);
         }
         $attr_string = 'class="' . implode(' ', array_unique($classes)) . '"';
-        $allowed_attrs = ['custom_margin', 'custom_padding', 'custom_css_main_element'];
+        $allowed_attrs = ['custom_margin', 'custom_padding', 'custom_css_main_element', 'hover_enabled', 'sticky_enabled'];
         foreach ($attrs as $key => $value) {
             if (in_array($key, $allowed_attrs)) {
                 $attr_string .= " {$key}=\"{$value}\"";
